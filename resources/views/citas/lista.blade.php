67ff5724c4a1ff4b526ac7821d14ad5d3161a23e
Pablo Vacas <pablovacasgarcia@gmail.com>
2024-02-04 00:39:40 +0100

    2

--- resources/views/citas/lista.blade.php
+++ resources/views/citas/lista.blade.php
@@ -1,9 +1,8 @@
 <x-app-layout>
-    <a href="/citas/create">Nuevo Marca</a>
     <table>
         <tr>
             <th>Fecha</th>
-            <th>Cliente</th>
+            <th>ID Cliente</th>
             <th>Acciones</th>
         </tr>
         @foreach ($citas as $cita)
