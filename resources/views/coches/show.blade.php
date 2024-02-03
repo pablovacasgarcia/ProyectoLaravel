23a71e664f13693bec36e8953c1518b12ccda110
Pablo Vacas <pablovacasgarcia@gmail.com>
2024-02-03 23:45:50 +0100

    euros

--- resources/views/coches/show.blade.php
+++ resources/views/coches/show.blade.php
@@ -1,5 +1,5 @@
 <x-app-layout>
     <h2>{{$datos['modelo']}}</h2>
-    <h3>{{$datos['precio']}}</h3>
+    <h3>{{$datos['precio']}}€</h3>
     <img src="../img/{{$datos['imagen']}}" alt="coche">
 </x-app-layout>
