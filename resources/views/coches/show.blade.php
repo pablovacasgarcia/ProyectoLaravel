3b7c931fc38fec5fca9414d8c9aeca04cba535cd
Pablo Vacas <pablovacasgarcia@gmail.com>
2024-02-03 23:40:47 +0100

    Detalles coche

--- resources/views/coches/show.blade.php
+++ resources/views/coches/show.blade.php
@@ -1,5 +1,5 @@
 <x-app-layout>
     <h2>{{$datos['modelo']}}</h2>
     <h3>{{$datos['precio']}}</h3>
-    <img src="" alt="coche">
+    <img src="../img/{{$datos['imagen']}}" alt="coche">
 </x-app-layout>
