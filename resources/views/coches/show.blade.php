cc3138b2670ad35cfb5a0c951e6efae378c8023f
MiguelHurtado <mhurtadojunior@gmail.com>
2024-02-05 13:05:02 +0100

    Dando estilos

--- resources/views/coches/show.blade.php
+++ resources/views/coches/show.blade.php
@@ -1,6 +1,9 @@
 <x-app-layout>
-    <h2>{{$datos['modelo']}}</h2>
-    <h3>{{$datos['precio']}}€</h3>
-    <img src="../img/{{$datos['imagen']}}" alt="coche" width="100px">
-    <a href="{{ route('citas.create', ['coche_id' => $datos['id']]) }}">Reservar cita</a>
+    <div>
+        <h2>{{$datos['modelo']}}</h2>
+        <h3>{{$datos['precio']}}€</h3>
+        <img src="../img/{{$datos['imagen']}}" alt="coche" width="100px">
+        <a href="{{ route('citas.create', ['coche_id' => $datos['id']]) }}">Reservar cita</a>
+    </div>
+    
 </x-app-layout>
--- resources/views/marca/coches.blade.php
+++ resources/views/marca/coches.blade.php
@@ -1,10 +1,14 @@
 <x-app-layout>
     @foreach($coches as $coche)
         <x-nav-link class="coche" :href="route('coches.show', $coche['id'])">
-            <div class="coche">
-                <img src="../img/{{$coche['imagen']}}" alt="coche">
-                <p>{{$coche['modelo']}}</p>
+            <div class="cocheDatos">
+                <h2>{{$coche['modelo']}}</h2>
+                <p>Desde: {{$coche['precio']}}€</p>
             </div>
+            <div class="cocheImagen">
+                <img src="../img/{{$coche['imagen']}}" alt="coche">
+            </div>
+                
         </x-nav-link>
     @endforeach
 </x-app-layout>
