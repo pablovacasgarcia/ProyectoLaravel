0ce6f209163b8ab76c98a43bc91b25f22d55e3cd
Pablo Vacas <pablovacasgarcia@gmail.com>
2024-02-05 13:29:09 +0100

    Estilos detalles

--- resources/views/coches/show.blade.php
+++ resources/views/coches/show.blade.php
@@ -1,9 +1,8 @@
 <x-app-layout>
-    <div>
+    <div class="detallesCoche">
         <h2>{{$datos['modelo']}}</h2>
         <h3>{{$datos['precio']}}€</h3>
         <img src="../img/{{$datos['imagen']}}" alt="coche" width="100px">
         <a href="{{ route('citas.create', ['coche_id' => $datos['id']]) }}">Reservar cita</a>
     </div>
-    
 </x-app-layout>
