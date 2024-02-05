987ba4215bbabf17bb89073b85da7cc924983fa2
MiguelHurtado <mhurtadojunior@gmail.com>
2024-02-05 13:11:39 +0100

    Dando estilos

--- resources/views/marca/coches.blade.php
+++ resources/views/marca/coches.blade.php
@@ -1,14 +1,16 @@
 <x-app-layout>
-    @foreach($coches as $coche)
-        <x-nav-link class="coche" :href="route('coches.show', $coche['id'])">
-            <div class="cocheDatos">
-                <h2>{{$coche['modelo']}}</h2>
-                <p>Desde: {{$coche['precio']}}€</p>
-            </div>
-            <div class="cocheImagen">
-                <img src="../img/{{$coche['imagen']}}" alt="coche">
-            </div>
-                
-        </x-nav-link>
-    @endforeach
+    <div class="coches">
+        @foreach($coches as $coche)
+            <x-nav-link class="coche" :href="route('coches.show', $coche['id'])">
+                <div class="cocheDatos">
+                    <h2>{{$coche['modelo']}}</h2>
+                    <p>Desde: {{$coche['precio']}}€</p>
+                </div>
+                <div class="cocheImagen">
+                    <img src="../img/{{$coche['imagen']}}" alt="coche">
+                </div>
+                    
+            </x-nav-link>
+        @endforeach
+    </div>
 </x-app-layout>
