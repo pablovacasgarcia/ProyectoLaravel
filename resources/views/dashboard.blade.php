aaf50dbe2d72d6b6f7fa4389a9a00df4cc89583d
MiguelHurtado <mhurtadojunior@gmail.com>
2024-02-06 12:02:19 +0100

    FINISH

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -339,13 +339,13 @@
     <!-- ANIMATION -->
     <script>
         /*TITLE Y IMG*/
-        gsap.from('.home__title', {opacity: -1, duration: 1.5, delay:1})
-        gsap.from('.home__img', {opacity: 0, duration: 1, delay:1, x: 100})
-        gsap.from('.home__information', {opacity: 0, duration: 1, delay:1.2, y: -30})
+        gsap.from('.home__title', {opacity: -1, duration: 1.5, delay:0.5})
+        gsap.from('.home__img', {opacity: 0, duration: 1, delay:0.5, x: 100})
+        gsap.from('.home__information', {opacity: 0, duration: 1, delay:0.5, y: -30})
 
         /*BOX*/
-        gsap.from('.box__medium', {opacity: 0, duration: 1.5, delay:1.3, y: -50})
-        gsap.from('.box__small', {opacity: -1, duration: 1.5, delay:1.4, y: -50})
+        gsap.from('.box__medium', {opacity: 0, duration: 1.5, delay:0.8, y: -50})
+        gsap.from('.box__small', {opacity: -1, duration: 1.5, delay:0.9, y: -50})
     </script>
 
     <script>
