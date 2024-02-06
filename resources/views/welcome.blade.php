7f47d016e01ebb95493076163a24d90a30e5a63a
MiguelHurtado <mhurtadojunior@gmail.com>
2024-02-06 11:58:24 +0100

    Proyecto TERMINADO

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,12 +4,15 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
+        
+        <link rel="icon" href='../img/PMJLogoFondo.png' type="image/png">
+
         <title>PMJ Luxury</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
-        <link rel="shortcut icon" href="../img/gearLogo.png" type="image/x-icon">
+        
         <link
             rel="stylesheet"
             href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/css/bootstrap.min.css"
