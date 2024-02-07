5691f3bc6cab57e479834c0123c9e87d0b02028c
Pablo Vacas <pablovacasgarcia@gmail.com>
2024-02-07 12:06:49 +0100

    Cambiado title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>PMJ Luxury</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
@@ -30,7 +30,7 @@
             <!-- Page Content -->
             <main>
                 {{ $slot }}
-                
+
             </main>
         </div>
     </body>
